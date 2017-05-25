37bb714224dd2838d079e17beecf9f43d81ab1b8
ivanlardis <ivan24031993@yandex.ru>
2017-05-25 14:42:43 +0300

    - старт задания

---
+++ app/src/main/java/com/lardis/ivan/newsreader/NavigationActivity.java
@@ -0,0 +1,13 @@
+package com.lardis.ivan.newsreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation_activity);
+    }
+}
